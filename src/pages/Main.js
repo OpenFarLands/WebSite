79c74021c87b78e6ad66017c3fa5e8b667b3c732
mikhaillav <mikhaillavelin@gmail.com>
2023-08-04 18:18:44 +0300

    fix link

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -28,7 +28,7 @@ class MainPage extends React.Component {
 
                         <p class="mb-3 font-normal text-gray-700">Не смотря на гриферов и отсутствие приватов - вы можете дать волю фантазии, и построить поистине красивые и уникальные постройки. Главное не забывать отходить подальше от спавна и носить головы мобов.</p>
                         
-                        <a href="minecraft:?addExternalServer=§a§lFarlands|far-lands.top:19132" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        <a href="minecraft://?addExternalServer=%C2%A7a%C2%A7lFarlands|far-lands.top:19132" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                             Зайти на сервер
                             <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
